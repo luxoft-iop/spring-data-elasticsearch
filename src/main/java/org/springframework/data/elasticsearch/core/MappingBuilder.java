f15d25c9f486a81c3d82c3b06fb54cec2d6e1691
luxoft-iop <msydorov@luxoft.com>
2013-12-01 17:30:03 +0200

    Revert @Id and @parentId fields are stored twice

--- src/main/java/org/springframework/data/elasticsearch/core/MappingBuilder.java
+++ src/main/java/org/springframework/data/elasticsearch/core/MappingBuilder.java
@@ -16,8 +16,6 @@
 package org.springframework.data.elasticsearch.core;
 
 import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.elasticsearch.annotations.*;
 import org.springframework.data.elasticsearch.core.facet.FacetRequest;
 import org.springframework.data.elasticsearch.core.mapping.ElasticsearchPersistentEntity;
@@ -92,9 +90,6 @@ class MappingBuilder {
         }
 
         for (java.lang.reflect.Field field : fields) {
-            if (field.isAnnotationPresent(Transient.class) || field.isAnnotationPresent(Id.class) || field.isAnnotationPresent(ParentId.class)) {
-                continue;
-            }
             if (isEntity(field)) {
                 mapEntity(xContentBuilder, field.getType(), false, EMPTY, field.getName());
             }
